5654062c04fefc39041be79cb60c06d33d31dfa1
jake jordan <jakesjordan11@gmail.com>
2023-02-27 10:01:45 -0700

    🔨 selfclose script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <script async src="http://localhost:8097"></script>
+      <script async src="http://localhost:8097" />
       <body>{children}</body>
     </html>
   );
