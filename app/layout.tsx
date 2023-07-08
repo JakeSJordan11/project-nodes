3c295c59f9fdb61b2306a2a262fb9a80b05483dc
Jake Jordan <jakesjordan11@gmail.com>
2023-07-08 05:35:37 -0600

    remove react dev tools script

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
-      <script async src="http://localhost:8097" />
       <body>{children}</body>
     </html>
   );
