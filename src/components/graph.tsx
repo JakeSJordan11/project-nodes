e913b4813ea2cd6c4529c99f3b09304d69a42812
Jake Jordan <jakesjordan11@gmail.com>
2023-11-30 14:08:10 -0700

    moved events to functions for consistency

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -2,26 +2,35 @@ import { ContextMenu } from '@/components/graph.menu'
 import { Stream } from '@/components/stream'
 import { useGraph } from '@/hooks/graphs.context'
 import styles from '@/styles/graph.module.css'
+import { PointerEvent } from 'react'
 import { Node } from './node'
 
 export default function Graph() {
   const { state, dispatch } = useGraph()
 
+  function handlePointerMove(event: PointerEvent<HTMLDivElement>) {
+    dispatch({ type: 'graph_pointer_move', payload: { event: event } })
+  }
+
+  function handlePointerUp(event: PointerEvent<HTMLDivElement>) {
+    dispatch({ type: 'graph_pointer_up', payload: { event: event } })
+  }
+
+  function handlePointerLeave(event: PointerEvent<HTMLDivElement>) {
+    dispatch({ type: 'graph_pointer_leave', payload: { event: event } })
+  }
+
+  function handleContextMenu(event: PointerEvent<HTMLDivElement>) {
+    dispatch({ type: 'graph_menu_show', payload: { event: event } })
+  }
+
   return (
     <main
       className={styles.main}
-      onPointerMove={(event) =>
-        dispatch({ type: 'graph_pointer_move', payload: { event: event } })
-      }
-      onPointerUp={(event) =>
-        dispatch({ type: 'graph_pointer_up', payload: { event: event } })
-      }
-      onPointerLeave={(event) =>
-        dispatch({ type: 'graph_pointer_leave', payload: { event: event } })
-      }
-      onContextMenu={(event) =>
-        dispatch({ type: 'graph_menu_show', payload: { event: event } })
-      }
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
+      onPointerLeave={handlePointerLeave}
+      onContextMenu={handleContextMenu}
     >
       {state.nodes.map((node) => (
         <Node key={node.id} {...node} />
