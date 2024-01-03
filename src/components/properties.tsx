9afb7a7c3ff07325e03f5d0168921a3e8986c44b
Jake Jordan <jakesjordan11@gmail.com>
2024-01-02 22:57:53 -0700

    wip: changes to layout styles

--- src/components/properties.tsx
+++ src/components/properties.tsx
@@ -1,5 +1,9 @@
 import styles from '@/styles/properties.module.css'
 
 export function Properties() {
-  return <article className={styles.properties}>properties</article>
+  return (
+    <article className={styles.properties}>
+      <h1 className={styles.title}>properties</h1>
+    </article>
+  )
 }
