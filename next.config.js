ebe66754a8cd3c74d6093fbd8e22d9d35354f2e1
Jake Jordan <JakeSJordan11@Gmail.com>
2024-09-16 12:29:59 -0600

    removed jest

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.wgsl$/i,
